5c91638107fa77a833df9d31137411866e5677c1
th3s4ms3piol <th3s4ms3piol@gmail.com>
2022-09-16 11:44:29 -0700

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,14 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 			return (0);
 }
