9e0fcb2492369af06e49ceccc8da29e20a66a8df
th3s4ms3piol <th3s4ms3piol@gmail.com>
2022-09-19 15:29:03 -0700

    3

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,26 +1,30 @@
 /*
- *  * File: 4-print_alphabt.c
- *   * Auth: Brennan D Baraban
- *    */
+ * File: 4-print_alphabt.c
+ * Auth: Brennan D Baraban
+ */
 
 #include <stdio.h>
 
 /**
- *  * main - Prints the alphabet in lowercase, except for q and e.
- *   *
- *    * Return: Always 0.
- *     */
+ * main - Prints the alphabet in lowercase, except for q and e.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-		char letter;
+	char letter;
 
-			for (letter = 'a'; letter <= 'z'; letter++)
-					{
-								if (letter != 'e' && letter != 'q')
-												putchar(letter);
-									}
+	for (letter = 'a'; letter <= 'z'; letter++)
 
-				putchar('\n');
+	{
 
-					return (0);
+	if (letter != 'e' && letter != 'q')
+		putchar(letter);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
 }
