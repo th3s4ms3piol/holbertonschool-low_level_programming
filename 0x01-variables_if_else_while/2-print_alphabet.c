90f92140ee5bf3bfb17c2d694deede0635e4aff8
th3s4ms3piol <th3s4ms3piol@gmail.com>
2022-09-19 15:23:26 -0700

    2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,23 +1,25 @@
 /*
- *  * File: 2-print_alphabet.c
- *   * Auth: Brennan D Baraban
- *    */
+ * File: 2-print_alphabet.c
+ * Auth: Brennan D Baraban
+ */
 
 #include <stdio.h>
 
 /**
- *  * main - Prints the alphabet in lowercase.
- *   *
- *    * Return: Always 0.
- *     */
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+
 int main(void)
 {
-		char letter;
+	char letter;
 
-			for (letter = 'a'; letter <= 'z'; letter++)
-						putchar(letter);
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
 
-				putchar('\n');
+	putchar('\n');
 
-					return (0);
+	return (0);
+
 }
