fe14eff3e4a1f8f36c150a69d30de833f57a53aa
th3s4ms3piol <th3s4ms3piol@gmail.com>
2022-09-20 09:42:57 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet
+ *
+ * follow new
+ *
+ * return: a
+ */
+
+void print_alphabet(void)
+{
+
+	char letter;
+
+	for (letter = 'a'; letter <= '2'; letter++)
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
+}
