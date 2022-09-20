5037f56b581c9e41e76b590dc326adf8d4beb998
th3s4ms3piol <th3s4ms3piol@gmail.com>
2022-09-20 08:58:56 -0700

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,13 @@
-#include "_putchar"
+#include <_putchar>
 /**
  * main - prints the string "_putchar" from a character array.
  * Return: 0 on success.
  */
-int main(void)
+int main(void
+		)
 {
 	char c[] = "_putchar";
+
 	int i;
 
 	for (i = 0; i < 9; i++)
