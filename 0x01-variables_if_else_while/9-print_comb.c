135d7be33e1da5914e3ab74c1fcc7d8d9e53dea7
th3s4ms3piol <th3s4ms3piol@gmail.com>
2022-09-20 12:28:46 -0700

    9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,32 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
  * main - Enter if you will
- *
  * Return: Always 0.
  */
-
 int main(void)
 {
 	int num = 48;
-
 	while (num < 58)
 	{
-
 		putchar(num);
+
 			if (num < 57)
 			{
 												
 				putchar(',');
+			
 				putchar(' ');
+			
 			}
 		num++;
 
 	}
-
 		putchar('\n');
+
 		return (0);
 
 }
