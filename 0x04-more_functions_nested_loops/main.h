071b84350407bbb2508f51bc54ff55badfd8f634
th3s4ms3piol <th3s4ms3piol@gmail.com>
2022-09-23 08:31:33 -0700

    0

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#indef HEADER_MAIN
+#define HEADER_MAIN
+
+int _isupper(int c);
+int _isdigit (int c);
+int mul(int a, int b);
+void print _numbers (void);
+void print_most _numbers (void);
+void more_numbers (void);
+void print line (int n);
+void print_ diagonal(int n);
+void print_square (int size);
+void print_triangle(int size;
+
+#endif
