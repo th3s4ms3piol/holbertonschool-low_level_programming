224bc717d0060deaf6f704ca27c9954fbde3b735
th3s4ms3piol <th3s4ms3piol@gmail.com>
2022-09-23 08:50:00 -0700

    0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #indef HEADER_MAIN
 #define HEADER_MAIN
 
+int _putchar(main c)
 int _isupper(int c);                                                                                                                            
 int _isdigit (int c);                                                                                                                           
 int mul(int a, int b);                                                                                                                          
