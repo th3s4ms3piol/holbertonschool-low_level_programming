98fb35dbf2444da7681aae75df3ab19f7fbbcd10
th3s4ms3piol <th3s4ms3piol@gmail.com>
2022-09-24 16:09:59 -0700

    5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - prin that sign function
+ *@n: #put
+ *Return: 1 print 
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar ('+');
+			return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar ('0');
+		return (0);
+	}
+	else
+	{
+		_putchar ('-');
+		return (-1);
+	}
+}
