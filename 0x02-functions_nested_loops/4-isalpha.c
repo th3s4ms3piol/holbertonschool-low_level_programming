6a56c5b41d860b0bcf3c47a8fe222c404dbcb283
th3s4ms3piol <th3s4ms3piol@gmail.com>
2022-09-24 12:35:54 -0700

    4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include
+/**
+ * _isalpha - check it
+ * @c: singular putter
+ * Return: 1 = c <> - 0 though
+ */
+
+int _isalpha(int c)
+{
+	if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
+		return (1);
+	else
+		return (0);
+}
