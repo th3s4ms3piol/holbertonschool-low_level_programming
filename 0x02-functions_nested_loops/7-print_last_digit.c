4f675b15d937a5e8eed9f32b672709cb1c3fce74
th3s4ms3piol <th3s4ms3piol@gmail.com>
2022-09-24 16:24:27 -0700

    7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.c"
+/**
+ *print_last_digit - prtins the last digit
+ *@nld: last
+ *Return: last digit returned
+ */
+int print_last_digit(int nld)
+{
+	int pld;
+
+	pld = (nld % 10);
+	{
+		pld = (-1 * pld);
+	}
+
+	_putchar(pld + '0');
+	return (pld)
+}
