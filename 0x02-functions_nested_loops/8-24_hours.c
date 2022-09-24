d1509f26d8497c0130f617a97e818102167a84f1
th3s4ms3piol <th3s4ms3piol@gmail.com>
2022-09-24 16:37:49 -0700

    8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/* jack_bauer - function print every min of day, 24 hour clock
+ * h = hour, m = minute
+ * / 10
+ *
+ * for loop sliced before hitting 24:00
+ *
+ * Return: 24 hour clock line/ line
+ */
+void jack_bauer(void)
+{
+	int h, m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m =0; m < 60; m++)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putcahr((m % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
